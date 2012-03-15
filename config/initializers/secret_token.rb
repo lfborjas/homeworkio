7897f36228e60156a274aab401eae8f269e0ad47
lfborjas <luis.borjas@escolarea.com>
2012-03-15 12:38:32 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeworkio::Application.config.secret_token = 'cd956422fac51e6398534f76183e9463e0473d516be85f02a5f851aee8ad01044a28880465f4822f16ea7381fdf96e69a1a93f7534f181adeccf859a63ebce23'
