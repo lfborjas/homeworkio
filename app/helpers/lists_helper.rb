ba889ce99d2b130b7d00d2d2a10b6b8361aa3afd
lfborjas <luis.borjas@escolarea.com>
2012-03-23 20:33:02 -0600

    more fancy presenting of stats: student sub count

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,13 @@
 module ListsHelper
+  def label_for_student(unique_subs, total_subs)
+    if unique_subs < total_subs
+      'important'
+    else
+      'success'
+    end
+  end
+  
+  def display_for_student(unique_subs, total_subs)
+    "#{unique_subs}/#{total_subs}"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,14 @@
 class Student < ActiveRecord::Base
   belongs_to :list
   belongs_to :student
-
+  has_many :submissions
   #find or create a student within a homework's list
   def self.from_address(address, homework)
     homework.list.students.find_or_create_by_email(address)
+  end
+
+  #All unique submissions for the list
+  def unique_submissions
+    submissions.select('distinct(homework_id)')
   end
 end
