cc29bb46d2ac50e18c3d30d454f445675a8ccad9
Luis Borjas <luisfborjas@gmail.com>
2012-03-27 02:41:20 -0600

    added some notes, updated some of the submit manual test

--- manual_tests/submit_homework.rb
+++ manual_tests/submit_homework.rb
@@ -3,11 +3,11 @@ require 'mail'
 
 mail = Mail.new do 
     from "zalgo@zalgo.com"
-    to "8193977fa86911b60fe2+homework10@cloudmailin.net"
+    to "submit+homework10@homeworkio.com"
     subject "testing this out"
     body "Oh hai teacher"
     add_file "/home/lfborjas/workspace/homeworkio/manual_tests/imagine.txt"
 end
 
 
-RestClient.post "http://localhost:3000/incoming", :message => mail.to_s
+RestClient.post "http://localhost:3000/incoming_mime",  "mime-body" => mail.to_s
