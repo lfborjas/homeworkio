9b8e20f783eda2d4ae44cd84f478f2bb3b777450
Luis Borjas <luisfborjas@gmail.com>
2012-04-13 21:45:27 -0600

    generalized the manual submission script

--- manual_tests/submit_homework.rb
+++ manual_tests/submit_homework.rb
@@ -5,15 +5,12 @@ require 'socket'
 is_fernando = Socket.gethostname == 'fernando-dell' 
 
 mail = Mail.new do 
-    from "zalgo@zalgo.com"
-    to is_fernando ? "submit+homework1@homeworkio.com" : 
-    	"submit+homework10@homeworkio.com"
+    from ARGV[0] || "zalgo@zalgo.com"
+    to ARGV[1] || "submit+homework1@homeworkio.com"
     subject "testing this out"
     body "Oh hai teacher"
-    add_file is_fernando ? 
-    	"/home/fernando/Proyectos/Rails/homeworkio/manual_tests/imagine.txt" : 
-    	"/home/lfborjas/workspace/homeworkio/manual_tests/imagine.txt"
+    add_file "#{Dir.pwd}/manual_tests/imagine.txt"
 end
 
 
-RestClient.post "http://localhost:3000/incoming_mime",  "body-mime" => mail.to_s
+RestClient.post "http://#{is_fernando ? "localhost:3000" : "homeworkio.dev"}/incoming_mime",  "body-mime" => mail.to_s
